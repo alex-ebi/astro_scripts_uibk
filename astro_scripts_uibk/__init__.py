bf5b1b891cf063078fa59dbeb900bdef1d498b84
Alexander Ebenbichler <alexander@ebenbichler.net>
2025-11-18 14:26:28 -0500

    made .toml file

--- astro_scripts_uibk/__init__.py
+++ astro_scripts_uibk/__init__.py
@@ -1,4 +1,3 @@
-from ._version import get_versions
 from . import alignment
 from . import convolve
 from . import data_handling
@@ -11,9 +10,6 @@ from . import spectrum_reduction
 from . import stellar_modelling
 from . import transformations
 from . import widgets
-
-__version__ = get_versions()['version']
-del get_versions
 
 __all__ = ['alignment', 'convolve', 'data_handling', 'io_asu', 'photometry', 'pub_plot', 'query', 'spas',
            'spectrum_reduction', 'stellar_modelling', 'transformations', 'widgets']
