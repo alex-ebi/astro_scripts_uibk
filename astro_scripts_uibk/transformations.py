340122613e406418099e468d73208c990b65d79b
Alexander Ebenbichler <alexander@ebenbichler.net>
2025-11-27 11:57:37 -0500

    bary_corr with header info

--- astro_scripts_uibk/transformations.py
+++ astro_scripts_uibk/transformations.py
@@ -105,7 +105,7 @@ def rv_to_wavelength(rv: np.array, ref_wavelength: float) -> np.array:
     return np.array(ref_wavelength / gamma)
 
 
-def bary_corr(wavelength: np.array, star_name: str = None, obs_name: str = None, obs_location: list = None,
+def bary_corr(wavelength: np.array, star_name: str = None, header = None, obs_name: str = 'paranal', obs_location: list = None,
               obs_time=None,
               time_format: str = None, return_bc_rv: bool = False, silent=False) -> np.array:
     """
@@ -119,6 +119,9 @@ def bary_corr(wavelength: np.array, star_name: str = None, obs_name: str = None,
 
     star_name : str
         Simbad name of the target.
+
+    header : dict
+        Fits header. If this header is present, the function gets the star name, time_format and obs_time from the header.
 
     obs_name : str
         Name of the observatory.
@@ -146,6 +149,11 @@ def bary_corr(wavelength: np.array, star_name: str = None, obs_name: str = None,
         1D-Wavelength array in barycentric rest frame.
     """
     wavelength = np.array(wavelength)
+
+    if header is not None:
+        time_format = 'mjd'
+        obs_time = header['MJD-OBS']
+        star_name = header['OBJECT']
     # check if kwargs are missing, i.e. still None
     if star_name is None:
         raise ValueError('Missing star_name.')
