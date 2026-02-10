8371c5487e3c36ba05d6e389c091d975aac55813
Alexander Ebenbichler <alexander@ebenbichler.net>
2026-02-10 18:53:48 +0100

    dispersion with header as kwarg

--- astro_scripts_uibk/io_asu.py
+++ astro_scripts_uibk/io_asu.py
@@ -2,7 +2,7 @@ import numpy as np
 from astropy.io import fits
 
 
-def wave_from_dispersion(flux, start_wave, dispersion, crpix=0):
+def wave_from_dispersion(flux, start_wave=None, dispersion=None, crpix=0, hdr=None):
     """
     Calculates a wavelength array as a equidistant grid.
     Starts from 'star_wave' and makes further points with a constant separation 'dispersion'.
@@ -17,12 +17,19 @@ def wave_from_dispersion(flux, start_wave, dispersion, crpix=0):
         Wavelength step.
     crpix : int
         Index of reference pixel.
+    hdr : dict
+        
 
     Returns
     -------
     np.array
         Wavelength array matching to flux array in length.
     """
+    if hdr is not None:
+        start_wave = hdr['CRVAL1']
+        dispersion = hdr['CDELT1']
+        crpix = hdr['CRPIX1']
+
     index_col = np.array(range(len(flux)))
     index_col = index_col - crpix
     wave = index_col * dispersion + start_wave
